96d78bd650659687890261f68a1471941e01bc67
Jesús Gabriel <jesus15202009@gmail.com>
2022-08-26 14:32:35 -0500

    responsive navbar

--- src/components/layouts/Navbar/Navbar.tsx
+++ src/components/layouts/Navbar/Navbar.tsx
@@ -20,7 +20,8 @@ const Navbar = observer(({ UserStore }: PropsObserver) => {
     });
   }, []);
   return (
-        <nav className="navigator page_wrapper__nav relative">
+    <>
+        <nav className="navigator page_wrapper__nav">
               <ul className="navigator__list">
                 <li className="navigator__list__item">
                   <CustomLink to="/">
@@ -41,8 +42,8 @@ const Navbar = observer(({ UserStore }: PropsObserver) => {
                   </CustomLink>
                 </li>
               </ul>
-                <hr className="hr" />
-                <ul className="overflow-auto h-3/5 pt-6">
+                <hr className="hr hidden xl:block" />
+                <ul className="overflow-auto h-3/5 pt-6 hidden xl:block">
                     <li
                     className="mb-6 block text-sm text-gray-400 hover:underline hover:text-white"
                     key="me"
@@ -57,29 +58,55 @@ const Navbar = observer(({ UserStore }: PropsObserver) => {
                       className="mb-6 block text-sm text-gray-400 hover:underline hover:text-white"
                       key={playlist.id}
                       >
-                      <Link to={`/playlist/${playlist.id}`}>{playlist.name}</Link> </li>)
+                      <Link to={`/playlist/${playlist.id}`} className="block text-left truncate">{playlist.name}</Link> </li>)
                       : <p>You have no playlists yet</p>
                   }
                 </ul>
-                  {
+            </nav>
+            {
                     UserStore.user.images && UserStore.user.display_name &&
                     <button className="navigator__user element_wrapper" onClick={ () => setActiveDropdown(!activeDropDown)} ref={dropdownRef}>
                         <img className="navigator__user__img" src={UserStore.user.images[0].url || userDefaultImg}/>
-                        <p className="navigator__user__name text-lg ml-3">{UserStore.user.display_name}</p>
-                        <i className="fa fa-angle-right ml-8"></i>
+                        <p className="navigator__user__name text-sm ml-3 truncate xl:text-lg">{UserStore.user.display_name}</p>
+                        <i className="fa fa-angle-right ml-2 xl:ml-8"></i>
+                        <div className={`navigator__dropdown ${activeDropDown && 'active'}`}>
+                          <ul className="overflow-auto block h-2/3">
+                                    <li
+                              className="navigator__dropdown__list__item w-full block xl:hidden"
+                              key="me"
+                              >
+                                <Link to="/playlist/me" className="navigator__dropdown__list__button block p-3 text-left">Liked tracks</Link>
+                              </li>
+                              {
+                                UserStore.user.playlists &&
+                                UserStore.user.playlists.length > 0
+                                  ? UserStore.user.playlists.map((playlist: any) =>
+                                  <li
+                                  className="navigator__dropdown__list__item w-full block xl:hidden"
+                                  key={playlist.id}
+                                  >
+                                  <Link
+                                  to={`/playlist/${playlist.id}`}
+                                  className="navigator__dropdown__list__button block p-3 text-left truncate">
+                                    {playlist.name}
+                                    </Link>
+                                    </li>)
+                                  : <p>You have no playlists yet</p>
+                              }
+                          </ul>
+                          <hr className="block xl:hidden mt-4"/>
+                          <ul className="navigator__dropdown__list w-full block">
+                            <li className="">
+                            <Link to="/" className="navigator__dropdown__list__button block p-3 text-left">Profile</Link>
+                            </li>
+                            <li className="navigator__dropdown__list__item w-full">
+                              <button className="navigator__dropdown__list__button block p-3 w-full text-left" onClick={Logout}>Logout</button>
+                            </li>
+                          </ul>
+                        </div>
                     </button>
                   }
-                  <div className={`navigator__dropdown ${activeDropDown && 'active'}`}>
-                    <ul className="navigator__dropdown__list w-full">
-                      <li className="navigator__dropdown__list__item">
-                      <Link to="/" className="navigator__dropdown__list__button block p-3">Profile</Link>
-                      </li>
-                      <li className="navigator__dropdown__list__item w-full">
-                        <button className="navigator__dropdown__list__button block p-3 w-full text-left" onClick={Logout}>Logout</button>
-                      </li>
-                    </ul>
-                  </div>
-            </nav>
+    </>
   );
 });
 export default Navbar;
